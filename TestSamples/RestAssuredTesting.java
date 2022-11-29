527d793a01d817d84725bbe246e66be5bb32453c
Fob Hovhannisyan <fobhovhannisyan@AramayisHovhannisyan-QT.local>
2022-11-29 13:04:06 +0400

    initial commit

---
+++ TestSamples/RestAssuredTesting.java
@@ -0,0 +1,21 @@
+package TestSamples;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+public class RestAssuredTesting {
+
+    @Test
+    public void responseCheck() {
+
+        RestAssured.baseURI = "http://localhost:8063/api/sms/employee";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.queryParam("id","1").get();
+        response.prettyPrint();
+        String dateOfBirth = response.path("dateOfBirth");
+        System.out.println(dateOfBirth);
+
+    }
+}
